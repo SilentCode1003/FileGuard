3a95b2b6b761d871ad91d0abbb692a0d0338509d
nestortion <gerona.nestor@gmail.com>
2024-06-18 09:29:11 +0800

    auth
    connect mongoDB store for session

--- server/src/startup/routes.startup.ts
+++ server/src/startup/routes.startup.ts
@@ -1,8 +1,12 @@
 import type { Express } from 'express'
 import { authRouter } from '../routes/auth.route'
 import { healthRouter } from '../routes/health.route'
+import { auth } from '../middlewares/auth.middleware'
+import { fileRouter } from '../routes/file.route'
 
 export const initRoutes = (app: Express) => {
   app.use('/auth', authRouter)
+  app.use(auth)
   app.use('/health', healthRouter)
+  app.use('/file', fileRouter)
 }
