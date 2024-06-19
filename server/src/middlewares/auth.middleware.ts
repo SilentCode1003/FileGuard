e70dd68f06818c0a64f97a899b7188fba3f9a91a
nestortion <gerona.nestor@gmail.com>
2024-06-19 11:23:26 +0800

    auth
    change serviceAuth middleware to login then check if user exists

--- server/src/middlewares/auth.middleware.ts
+++ server/src/middlewares/auth.middleware.ts
@@ -1,5 +1,6 @@
 import type { RequestHandler } from 'express'
 import { prisma } from '../db/prisma'
+import { loginSchema } from '../schema/auth.schema'
 
 export const auth: RequestHandler = async (req, res, next) => {
   if (!req.session.user) {
@@ -14,13 +15,18 @@ export const auth: RequestHandler = async (req, res, next) => {
 }
 
 export const serviceAuth: RequestHandler = async (req, res, next) => {
-  if (!req.session.user) {
-    return res.status(401).json({ message: 'Unauthorized' })
+  const validatedBody = await loginSchema.safeParseAsync({
+    username: req.body.username,
+    password: req.body.password,
+  })
+
+  if (!validatedBody.success) {
+    return res.status(400).json({ message: 'invalid credentials' })
   }
 
   const user = await prisma.users.findUnique({
     where: {
-      userId: req.session.user.userId,
+      userUsername: validatedBody.data.username,
     },
   })
 
