77b19ef2cd5973938000a7f92ab590507c1b9040
blancowendell <blanco.wendell15@gmail.com>
2024-06-20 14:35:10 +0800

    [company and companyDepartment]
    [add update and toggle]

--- server/src/schema/companyDepartment.schema.ts
+++ server/src/schema/companyDepartment.schema.ts
@@ -9,5 +9,26 @@ export const createCompanyDepartmentSchema = z.object({
 
 export const companyDepartmentIdSchema = z.object({
     id: z.string().nanoid(),
+})
+
+
+export const updateCompanyDepartmentSchema = z.object({
+    cdId: z
+      .string({ required_error: 'cdId is required' })
+      .trim()
+      .min(1, { message: 'cdId is required' }),
+    cdCompId: z.string().optional(),
+    cdDeptId: z.string().optional(),
   })
+  
+  
+  export const toggleCompanyDepartmentSchema = z.object({
+    cdId: z
+      .string({
+        required_error: 'cdId  is required',
+      })
+      .nanoid(),
+  })
+  
+  
   
