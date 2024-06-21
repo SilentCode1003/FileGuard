0be8a913454170b2f8b1d582435251f97d0cab6e
nestortion <gerona.nestor@gmail.com>
2024-06-21 09:40:28 +0800

    schema
    revert change #1c14744

--- server/src/schema/files.schema.ts
+++ server/src/schema/files.schema.ts
@@ -3,12 +3,23 @@ import { z } from 'zod'
 export const createFileSchema = z.object({
   files: z.array(
     z.object({
-      file: z.string({ required_error: 'file is required' }).base64(),
+      file: z
+        .string({ required_error: 'file is required' })
+        .refine((value) => isValidBase64(value), 'file is not a valid base64 string'),
       fileName: z.string({ required_error: 'fileName is required' }),
       filePath: z.string({ required_error: 'filePath is required' }),
+      fileMimeType: z.string({ required_error: 'fileMimeType is required' }),
     }),
   ),
 })
+
+const isValidBase64 = (b: string) => {
+  try {
+    return b.length > 3 && !(b.length % 4) && atob(b) && true
+  } catch (err) {
+    return false
+  }
+}
 
 export const searchFilesSchema = z.object({
   searchText: z.string().optional(),
@@ -29,6 +40,7 @@ export const createRevisionsSchema = z.object({
       revFileName: z.string({ required_error: 'revFileName is required' }),
       revFilePath: z.string({ required_error: 'revFilePath is required' }),
       revFileId: z.string({ required_error: 'revFileId is required' }).nanoid(),
+      revFileMimeType: z.string({ required_error: 'revFileMimeType is required' }),
     }),
   ),
 })
