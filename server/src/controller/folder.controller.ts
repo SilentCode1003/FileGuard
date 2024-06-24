3e547bd829e967e9c1cca47de4982d0e48646b5a
nestortion <gerona.nestor@gmail.com>
2024-06-24 14:32:10 +0800

    task
    add getFoldersByParentFolderId

--- server/src/controller/folder.controller.ts
+++ server/src/controller/folder.controller.ts
@@ -3,11 +3,15 @@ import { existsSync } from 'fs'
 import { mkdir } from 'fs/promises'
 import { CONFIG } from '../config/env.config'
 import { prisma } from '../db/prisma'
-import { createFolderSchema, getFoldersSchema } from '../schema/folder.schema'
+import {
+  createFolderSchema,
+  getFoldersByParentIdSchema,
+  getFoldersByPathSchema,
+} from '../schema/folder.schema'
 import { nanoid } from '../util/nano.util'
 
-export const getFolders: RequestHandler = async (req, res) => {
-  const validatedBody = getFoldersSchema.safeParse(req.query)
+export const getFoldersByPath: RequestHandler = async (req, res) => {
+  const validatedBody = getFoldersByPathSchema.safeParse(req.query)
 
   if (!validatedBody.success) {
     return res.status(400).json({ message: validatedBody.error.errors[0]?.message })
@@ -16,7 +20,29 @@ export const getFolders: RequestHandler = async (req, res) => {
   try {
     const folders = await prisma.folders.findMany({
       where: {
-        folderPath: `${validatedBody.data.folderPath}`,
+        folderPath: validatedBody.data.folderPath,
+      },
+    })
+    return res.status(200).json({ data: folders })
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(400).json({ message: error.message })
+    }
+    return res.status(500).json({ message: error })
+  }
+}
+
+export const getFoldersByParentId: RequestHandler = async (req, res) => {
+  const validatedBody = getFoldersByParentIdSchema.safeParse(req.query)
+
+  if (!validatedBody.success) {
+    return res.status(400).json({ message: validatedBody.error.errors[0]?.message })
+  }
+
+  try {
+    const folders = await prisma.folders.findMany({
+      where: {
+        folderParentId: validatedBody.data.folderParentId,
       },
     })
     return res.status(200).json({ data: folders })
@@ -29,7 +55,6 @@ export const getFolders: RequestHandler = async (req, res) => {
 }
 
 export const createFolder: RequestHandler = async (req, res) => {
-  console.log(req.body)
   const validatedBody = createFolderSchema.safeParse({
     ...req.body,
     folderUserId: req.context.user!.userId,
@@ -48,7 +73,6 @@ export const createFolder: RequestHandler = async (req, res) => {
       if (!existsSync(folderPath)) {
         await mkdir(folderPath)
       }
-      console.log(validatedBody.data)
 
       const checkFolder = await prisma.folders.findFirst({
         where: {
