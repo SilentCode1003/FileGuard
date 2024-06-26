35c5d7a41bcc56a4ea714203a58cbfe90e34d570
nestortion <gerona.nestor@gmail.com>
2024-06-26 09:23:54 +0800

    feat
    implement preview file auth

--- server/src/middlewares/previewFile.middleware.ts
+++ server/src/middlewares/previewFile.middleware.ts
@@ -1,9 +1,45 @@
 import type { RequestHandler } from 'express'
+import { prisma } from '../db/prisma'
+import { getFolderPath } from '../util/customhelper'
 
 export const previewFileAuth: RequestHandler = async (req, res, next) => {
-  console.log(req.url)
+  try {
+    // check if file exists
 
-  // return res.status(401).json({ message: 'Unauthorized' })
+    const checkFile = await prisma.files.findFirst({
+      where: {
+        filePath: getFolderPath(req.url),
+      },
+    })
+    if (!checkFile) throw new Error('File not found!')
 
-  next()
+    // check user role
+    if (req.context.user.role.urName === 'admin') next()
+    else {
+      // check user permission
+
+      const permission = await prisma.permissions.findFirst({
+        where: {
+          AND: [
+            {
+              permCdId: req.context.user.userCdId,
+            },
+            {
+              permFolderId: checkFile.fileFolderId,
+            },
+          ],
+        },
+      })
+
+      if (!permission) throw new Error('Unauthorized!')
+
+      next()
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error.message)
+      res.statusMessage = error.message
+      return res.status(500).send({ message: error.message })
+    }
+  }
 }
